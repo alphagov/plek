alphagov/plek

commit 8de661c54e726338361875fa92ffd038bfebb181

updated name_for regex to allow subdomains and wrote tests

lib/plek.rb
@@ -164,7 +164,7 @@ class Plek
     name = service.to_s.dup
     name.downcase!
     name.strip!
-    name.gsub!(/[^a-z]+/, '')
+    name.gsub!(/[^a-z\.]+/, '')
     name
   end
 

test/plek_test.rb
@@ -79,4 +79,19 @@ class PlekTest < Test::Unit::TestCase
     tariff_url = Plek.new("preview").find("tariff")
     assert_equal "https://tariff.preview.alphagov.co.uk", tariff_url
   end
+
+  def test_should_return_subdomain_divided_source_in_dev
+    url = Plek.new("development").find("explore.reviewomatic")
+    assert_equal "http://explore.reviewomatic.dev.gov.uk", url
+  end
+
+  def test_should_return_subdomain_divided_source_in_preview
+    url = Plek.new("preview").find("explore.reviewomatic")
+    assert_equal "https://explore.reviewomatic.preview.alphagov.co.uk", url
+  end
+
+  def test_should_return_subdomain_divided_source_in_production
+    url = Plek.new("production").find("explore.reviewomatic")
+    assert_equal "https://explore.reviewomatic.production.alphagov.co.uk", url
+  end
 end

commit 890c317e33b5945810474c4e0e2230ee97ae74d5

'publication preview' and 'whitehall search' should use hypens
This fixes a regression caused in 8de661c54e726338361875fa92ffd038bfebb181 and 9fe1f4bab0025c41c283ee59b86a53b5200887d8
where keys that originally had their hypens stripped out can no-longer be matched correctly.

Eg Publisher looks for 'publication-preview' which originally was stripped to 'publicationpreview' and resolved to
'private-frontend'. The previous commits meant that 'publication-preview' resolved to 'publication-preview' and broke
preview links & fact check email links.  Whitehall search would have encountered similar issues when updated to use
the latest version of plek.

Additionally, as those commits would have resulted in a change of behaviour, this should have been a minor version
bump not a patch.

lib/plek.rb
@@ -12,9 +12,9 @@ class Plek
     "production.arbiter"             => "panopticon.production.alphagov.co.uk",
     "production.assets"              => "static.production.alphagov.co.uk",
     "production.cdn"                 => "d17tffe05zdvwj.cloudfront.net",
-    "production.publicationpreview"  => "private-frontend.production.alphagov.co.uk",
+    "production.publication-preview" => "private-frontend.production.alphagov.co.uk",
     "production.whitehall"           => "whitehall.production.alphagov.co.uk",
-    "production.whitehallsearch"     => "whitehall-search.production.alphagov.co.uk",
+    "production.whitehall-search"    => "whitehall-search.production.alphagov.co.uk",
     "production.#{DEFAULT_PATTERN}"  => "%s.production.alphagov.co.uk",
 
     "preview.frontend"               => "www.preview.alphagov.co.uk",
@@ -24,10 +24,10 @@ class Plek
     "preview.arbiter"                => "panopticon.preview.alphagov.co.uk",
     "preview.assets"                 => "static.preview.alphagov.co.uk",
     "preview.cdn"                    => "djb1962t8apu5.cloudfront.net",
-    "preview.publicationpreview"     => "private-frontend.preview.alphagov.co.uk",
+    "preview.publication-preview"    => "private-frontend.preview.alphagov.co.uk",
     "preview.tariff"                 => "tariff.preview.alphagov.co.uk",
     "preview.whitehall"              => "whitehall.preview.alphagov.co.uk",
-    "preview.whitehallsearch"        => "whitehall-search.preview.alphagov.co.uk",
+    "preview.whitehall-search"       => "whitehall-search.preview.alphagov.co.uk",
     "preview.#{DEFAULT_PATTERN}"     => "%s.preview.alphagov.co.uk",
 
     "staging.frontend"               => "demo.alphagov.co.uk",
@@ -38,7 +38,7 @@ class Plek
     "staging.arbiter"                => "panopticon.staging.alphagov.co.uk",
     "staging.assets"                 => "static.staging.alphagov.co.uk",
     "staging.assets"                 => "static.staging.alphagov.co.uk",
-    "staging.publicationpreview"     => "private-frontend.staging.alphagov.co.uk",
+    "staging.publication-preview"    => "private-frontend.staging.alphagov.co.uk",
     "staging.#{DEFAULT_PATTERN}"     => "%s.staging.alphagov.co.uk",
 
     "development.authentication"     => "signon.dev.gov.uk",
@@ -47,17 +47,16 @@ class Plek
     "development.arbiter"            => "panopticon.dev.gov.uk",
     "development.assets"             => "static.dev.gov.uk",
     "development.cdn"                => "static.dev.gov.uk",
-    "development.publicationpreview" => "www.dev.gov.uk",
+    "development.publication-preview"=> "www.dev.gov.uk",
     "development.#{DEFAULT_PATTERN}" => "%s.dev.gov.uk",
 
     "test.authentication"            => "signon.test.gov.uk",
     "test.needs"                     => "needotron.test.gov.uk",
     "test.data"                      => "imminence.test.gov.uk",
     "test.arbiter"                   => "panopticon.test.gov.uk",
-    "test.publicationpreview"        => "www.test.gov.uk",
+    "test.publication-preview"       => "www.test.gov.uk",
     "test.cdn"                       => "static.test.gov.uk",
     "test.whitehall"                 => "whitehall.test.alphagov.co.uk",
-    "test.whitehallsearch"           => "whitehall-search.test.alphagov.co.uk",
     "test.whitehall-search"          => "whitehall-search.test.alphagov.co.uk",
     "test.#{DEFAULT_PATTERN}"        => "%s.test.gov.uk",
   }.freeze