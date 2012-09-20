49c51665651ebb2591738a0f9807d57e15810977
James Stewart <jys@ketlai.co.uk>
2012-09-20 12:07:28 +0100

    Remove now-redundant tests for old http behaviour

--- test/plek_test.rb
+++ test/plek_test.rb
@@ -35,26 +35,6 @@ class PlekTest < Test::Unit::TestCase
     assert_equal "whitehall-search.production.alphagov.co.uk", URI.parse(whitehall_search_url).host
   end
 
-  def test_should_return_whitehall_preview_host_url_as_non_ssl
-    whitehall_url = Plek.new("preview").find("whitehall")
-    assert_equal "http", URI.parse(whitehall_url).scheme
-  end
-
-  def test_should_return_whitehall_production_host_url_as_non_ssl
-    whitehall_url = Plek.new("production").find("whitehall")
-    assert_equal "http", URI.parse(whitehall_url).scheme
-  end
-
-  def test_should_return_whitehall_search_preview_host_url_as_non_ssl
-    whitehall_search_url = Plek.new("preview").find("whitehall-search")
-    assert_equal "http", URI.parse(whitehall_search_url).scheme
-  end
-
-  def test_should_return_whitehall_search_production_host_url_as_non_ssl
-    whitehall_search_url = Plek.new("production").find("whitehall-search")
-    assert_equal "http", URI.parse(whitehall_search_url).scheme
-  end
-
   def test_should_return_non_whitehall_preview_host_url_as_ssl
     url = Plek.new("preview").find("non-whitehall-service")
     assert_equal "https", URI.parse(url).scheme
@@ -67,11 +47,6 @@ class PlekTest < Test::Unit::TestCase
 
   def test_should_return_non_whitehall_non_preview_and_non_production_host_url_as_non_ssl
     url = Plek.new("development").find("non-whitehall-service")
-    assert_equal "http", URI.parse(url).scheme
-  end
-
-  def test_should_return_search_as_non_ssl
-    url = Plek.new("production").find("search")
     assert_equal "http", URI.parse(url).scheme
   end
 
