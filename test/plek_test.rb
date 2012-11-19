b94d3d501652f8ed60ca33e6ddaf94aac53aa6ca
Philip Potter <philipgpotter@gmail.com>
2012-11-19 17:56:38 +0000

    use GOVUK_APP_DOMAIN to find domain

--- test/plek_test.rb
+++ test/plek_test.rb
@@ -10,16 +10,6 @@ class PlekTest < Test::Unit::TestCase
     assert_equal "whitehall.test.gov.uk", URI.parse(whitehall_url).host
   end
 
-  def test_should_return_whitehall_preview_host_domain
-    whitehall_url = Plek.new("preview").find("whitehall")
-    assert_equal "whitehall.preview.alphagov.co.uk", URI.parse(whitehall_url).host
-  end
-
-  def test_should_return_whitehall_production_host_domain
-    whitehall_url = Plek.new("production").find("whitehall")
-    assert_equal "whitehall.production.alphagov.co.uk", URI.parse(whitehall_url).host
-  end
-
   def test_should_return_non_whitehall_preview_host_url_as_ssl
     url = Plek.new("preview").find("non-whitehall-service")
     assert_equal "https", URI.parse(url).scheme
@@ -35,39 +25,8 @@ class PlekTest < Test::Unit::TestCase
     assert_equal "http", URI.parse(url).scheme
   end
 
-  def test_should_return_tariff_preview_host_domain
-    tariff_url = Plek.new("preview").find("tariff")
-    assert_equal "https://tariff.preview.alphagov.co.uk", tariff_url
-  end
-
   def test_should_return_subdomain_divided_source_in_dev
     url = Plek.new("development").find("explore.reviewomatic")
     assert_equal "http://explore.reviewomatic.dev.gov.uk", url
   end
-
-  def test_should_return_subdomain_divided_source_in_preview
-    url = Plek.new("preview").find("explore.reviewomatic")
-    assert_equal "https://explore.reviewomatic.preview.alphagov.co.uk", url
-  end
-
-  def test_should_return_subdomain_divided_source_in_production
-    url = Plek.new("production").find("explore.reviewomatic")
-    assert_equal "https://explore.reviewomatic.production.alphagov.co.uk", url
-  end
-
-  def test_should_return_dash_divided_source_in_dev
-    url = Plek.new("development").find("explore-reviewomatic")
-    assert_equal "http://explore-reviewomatic.dev.gov.uk", url
-  end
-
-  def test_should_return_dash_divided_source_in_preview
-    url = Plek.new("preview").find("explore-reviewomatic")
-    assert_equal "https://explore-reviewomatic.preview.alphagov.co.uk", url
-  end
-
-  def test_should_return_dash_divided_source_in_production
-    url = Plek.new("production").find("explore-reviewomatic")
-    assert_equal "https://explore-reviewomatic.production.alphagov.co.uk", url
-  end
-
 end
