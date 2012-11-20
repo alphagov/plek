68e672438a95743e8007e803216b82321dbbc847
Dafydd Vaughan <dafydd.vaughan@digital.cabinet-office.gov.uk>
2012-11-20 12:19:03 +0000

    Remove all overrides and rely solely on convention and env variables
    This commit removes all of the domain name overrides such as 'cdn' and
    'publication-preview'. This reduces indirection and makes things 
    clearer.  
    
    Following discussions with @jystewart and @philandstuff, this
    commit also removes use of environment to determine the parent domain
    (e.g. preview.alphagov.co.uk) and instead uses the GOVUK_APP_DOMAIN 
    environment variable to set this.
    
    This commit breaks backwards compatibility with previous versions.

--- lib/plek.rb
+++ lib/plek.rb
@@ -3,61 +3,25 @@ require 'plek/version'
 
 class Plek
   DEFAULT_PATTERN = "pattern".freeze
-  DEFAULT_DOMAIN = ENV['GOVUK_APP_DOMAIN'] || 'dev.gov.uk'
+  DEFAULT_DOMAIN = ENV['GOVUK_APP_DOMAIN'] || 'test.gov.uk'
+  HTTP_DOMAINS = ['test.gov.uk', 'dev.gov.uk']
 
-  SERVICES = {
-    "production.www"                 => "www.gov.uk",
-    "production.assets"              => "static.production.alphagov.co.uk",
-    "production.cdn"                 => "d17tffe05zdvwj.cloudfront.net",
-    "production.publication-preview" => "private-frontend.production.alphagov.co.uk",
-    "production.#{DEFAULT_PATTERN}"  => "%s.#{DEFAULT_DOMAIN}",
+  attr_accessor :parent_domain
 
-    "preview.www"                    => "www.preview.alphagov.co.uk",
-    "preview.assets"                 => "static.preview.alphagov.co.uk",
-    "preview.cdn"                    => "djb1962t8apu5.cloudfront.net",
-    "preview.publication-preview"    => "private-frontend.preview.alphagov.co.uk",
-    "preview.#{DEFAULT_PATTERN}"     => "%s.#{DEFAULT_DOMAIN}",
-
-    "staging.frontend"               => "demo.alphagov.co.uk",
-    "staging.publisher"              => "guides.staging.alphagov.co.uk",
-    "staging.assets"                 => "static.staging.alphagov.co.uk",
-    "staging.publication-preview"    => "private-frontend.staging.alphagov.co.uk",
-    "staging.#{DEFAULT_PATTERN}"     => "%s.#{DEFAULT_DOMAIN}",
-
-    "development.assets"             => "static.dev.gov.uk",
-    "development.cdn"                => "static.dev.gov.uk",
-    "development.publication-preview"=> "www.dev.gov.uk",
-    "development.#{DEFAULT_PATTERN}" => "%s.#{DEFAULT_DOMAIN}",
-
-    "test.publication-preview"       => "www.test.gov.uk",
-    "test.cdn"                       => "static.test.gov.uk",
-    # FIXME: in the test environment, we should set GOVUK_APP_DOMAIN
-    # directly rather than govuk_platform
-    "test.#{DEFAULT_PATTERN}"        => "%s.test.gov.uk",
-  }.freeze
-
-  attr_accessor :environment
-  private :environment=
-
-  def initialize(environment)
-    self.environment = environment
+  def initialize(domain_to_use = nil)
+    self.parent_domain = domain_to_use || DEFAULT_DOMAIN
   end
 
   # Find the URI for a service/application.
   def find(service)
-    name = name_for service
-    host = SERVICES[service_key_for(name)]
-    host ||= SERVICES["#{environment}.#{DEFAULT_PATTERN}"].to_s % name
+    name = name_for(service)
+    host = "#{name}.#{parent_domain}"
 
-    if environment == 'preview' || environment == 'production'
-      "https://#{host}"
-    else
+    if HTTP_DOMAINS.include?(parent_domain)
       "http://#{host}"
+    else
+      "https://#{host}"
     end
-  end
-
-  def service_key_for(name)
-    "#{environment}.#{name}"
   end
 
   def name_for(service)
@@ -66,17 +30,5 @@ class Plek
     name.strip!
     name.gsub!(/[^a-z\.-]+/, '')
     name
-  end
-
-  def self.current_env
-    if (ENV['RAILS_ENV'] || ENV['RACK_ENV']) == 'test'
-      'test'
-    else
-      ENV['FACTER_govuk_platform'] || ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
-    end
-  end
-
-  def self.current
-    Plek.new(current_env)
   end
 end
--- test/plek_test.rb
+++ test/plek_test.rb
@@ -6,27 +6,68 @@ require "uri"
 
 class PlekTest < Test::Unit::TestCase
   def test_should_return_whitehall_test_host_domain
-    whitehall_url = Plek.new("test").find("whitehall")
+    whitehall_url = Plek.new("test.gov.uk").find("whitehall")
     assert_equal "whitehall.test.gov.uk", URI.parse(whitehall_url).host
   end
 
+  def test_should_return_whitehall_preview_host_domain
+    whitehall_url = Plek.new("preview.alphagov.co.uk").find("whitehall")
+    assert_equal "whitehall.preview.alphagov.co.uk", URI.parse(whitehall_url).host
+  end
+
+  def test_should_return_whitehall_production_host_domain
+    whitehall_url = Plek.new("production.alphagov.co.uk").find("whitehall")
+    assert_equal "whitehall.production.alphagov.co.uk", URI.parse(whitehall_url).host
+  end
+
   def test_should_return_non_whitehall_preview_host_url_as_ssl
-    url = Plek.new("preview").find("non-whitehall-service")
+    url = Plek.new("preview.alphagov.co.uk").find("non-whitehall-service")
     assert_equal "https", URI.parse(url).scheme
   end
 
   def test_should_return_non_whitehall_production_host_url_as_ssl
-    url = Plek.new("production").find("non-whitehall-service")
+    url = Plek.new("production.alphagov.co.uk").find("non-whitehall-service")
     assert_equal "https", URI.parse(url).scheme
   end
 
   def test_should_return_non_whitehall_non_preview_and_non_production_host_url_as_non_ssl
-    url = Plek.new("development").find("non-whitehall-service")
+    url = Plek.new.find("non-whitehall-service")
     assert_equal "http", URI.parse(url).scheme
   end
 
+  def test_should_return_tariff_preview_host_domain
+    tariff_url = Plek.new("preview.alphagov.co.uk").find("tariff")
+    assert_equal "https://tariff.preview.alphagov.co.uk", tariff_url
+  end
+
   def test_should_return_subdomain_divided_source_in_dev
-    url = Plek.new("development").find("explore.reviewomatic")
+    url = Plek.new.find("explore.reviewomatic")
     assert_equal "http://explore.reviewomatic.dev.gov.uk", url
   end
+
+  def test_should_return_subdomain_divided_source_in_preview
+    url = Plek.new("preview.alphagov.co.uk").find("explore.reviewomatic")
+    assert_equal "https://explore.reviewomatic.preview.alphagov.co.uk", url
+  end
+
+  def test_should_return_subdomain_divided_source_in_production
+    url = Plek.new("production.alphagov.co.uk").find("explore.reviewomatic")
+    assert_equal "https://explore.reviewomatic.production.alphagov.co.uk", url
+  end
+
+  def test_should_return_dash_divided_source_in_dev
+    url = Plek.new.find("explore-reviewomatic")
+    assert_equal "http://explore-reviewomatic.dev.gov.uk", url
+  end
+
+  def test_should_return_dash_divided_source_in_preview
+    url = Plek.new("preview.alphagov.co.uk").find("explore-reviewomatic")
+    assert_equal "https://explore-reviewomatic.preview.alphagov.co.uk", url
+  end
+
+  def test_should_return_dash_divided_source_in_production
+    url = Plek.new("production.alphagov.co.uk").find("explore-reviewomatic")
+    assert_equal "https://explore-reviewomatic.production.alphagov.co.uk", url
+  end
+
 end
