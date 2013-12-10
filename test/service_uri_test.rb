c183a849789d5c7f9cecc5fa22b558c62465f5cc
Jordan Hatch <jordan@jordanh.net>
2013-12-10 19:30:13 +0000

    Allow custom URIs for services to be defined
    Allow custom URIs for services to be defined using environment
    variables, so that it is easier to set up and run multiple
    services in isolation without them having to be running at
    particular hostnames.
    
    A service URI can be defined using the following environment
    variable naming convention:
    
        # replace <service> with the name of the service, uppercased
        # and underscored
        export PLEK_SERVICE_<service>_URI=http://localhost:3000
    
    Existing behaviour of the gem is not affected when these
    environment variables are not present.

---
+++ test/service_uri_test.rb
@@ -0,0 +1,30 @@
+require_relative "test_helper"
+
+describe Plek do
+  describe "overriding the uri for a service" do
+    before do
+      ENV.delete("PLEK_SERVICE_FOO_URI")
+    end
+
+    it "looks for an env variable matching the service name and returns its value when present" do
+      ENV["PLEK_SERVICE_FOO_URI"] = "http://foo.localhost:5001"
+      assert_equal "http://foo.localhost:5001", Plek.new().find("foo")
+    end
+
+    it "upcases and underscores the service name in the environment variable" do
+      ENV["PLEK_SERVICE_FOO_API_URI"] = "http://foo.localhost:5001"
+      assert_equal "http://foo.localhost:5001", Plek.new().find("foo-api")
+    end
+
+    it "falls back to regular behaviour when env variable is nil or empty" do
+      ENV["GOVUK_APP_DOMAIN"] = "dev.gov.uk"
+      ENV["PLEK_SERVICE_FOO_URI"] = nil
+      ENV["PLEK_SERVICE_BAR_URI"] = ""
+
+      assert_equal "http://foo.dev.gov.uk", Plek.new().find("foo")
+      assert_equal "http://bar.dev.gov.uk", Plek.new().find("bar")
+      assert_equal "http://baz.dev.gov.uk", Plek.new().find("baz") # not defined
+    end
+
+  end
+end
