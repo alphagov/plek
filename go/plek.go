52567dbedae16981c31afc6fd3e92df4c88ac2b8
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-01-29 16:06:34 +0000

    Add some godocs

--- go/plek.go
+++ go/plek.go
@@ -6,7 +6,9 @@ import (
 	"strings"
 )
 
+// An EnvVarMissing is returned when a required environment variable is missing
 type EnvVarMissing struct {
+	// The environment variable this relates to
 	EnvVar string
 }
 
@@ -14,9 +16,13 @@ func (e *EnvVarMissing) Error() string {
 	return "Expected " + e.EnvVar + " to be set. Perhaps you should run your task through govuk_setenv <appname>?"
 }
 
+// An EnvVarURLInvalid is returned when an environment variable does not
+// contain a valid URL.
 type EnvVarURLInvalid struct {
+	// The environment variable this relates to
 	EnvVar string
-	Err    error
+	// The error returned when parsing the URL.
+	Err error
 }
 
 func (e *EnvVarURLInvalid) Error() string {
@@ -27,6 +33,15 @@ var httpDomains = map[string]bool{
 	"dev.gov.uk": true,
 }
 
+// Find returns the base URL for the given service name in the default parent
+// domain.  The default domain is taken from the GOVUK_APP_DOMAIN environment
+// variable.  If this is unset, an EnvVarMissing error will be returned.
+//
+// The URLs for an individual service can be overridden by setting a
+// corresponding PLEK_SERVICE_FOO_URI environment variable.  For example, to
+// override the "foo-api" service url, set PLEK_SERVICE_FOO_API_URI to the base
+// URL of the service.  If it can't be parsed by url.Parse, an EnvVarURLInvalid
+// error will be returned.
 func Find(hostname string) (*url.URL, error) {
 	overrideURL, err := serviceURLFromEnvOverride(hostname)
 	if err != nil {
@@ -44,14 +59,17 @@ func Find(hostname string) (*url.URL, error) {
 	return Plek{parentDomain: appDomain}.Find(hostname), nil
 }
 
+// Plek builds service URLs for a given parent domain.
 type Plek struct {
 	parentDomain string
 }
 
+// New builds a new Plek instance for a given parent domain.
 func New(parentDomain string) Plek {
 	return Plek{parentDomain: parentDomain}
 }
 
+// Find returns the base URL for the given service name.
 func (p Plek) Find(serviceName string) *url.URL {
 	u := &url.URL{Scheme: "https", Host: serviceName + "." + p.parentDomain}
 	if httpDomains[p.parentDomain] {
@@ -60,10 +78,18 @@ func (p Plek) Find(serviceName string) *url.URL {
 	return u
 }
 
+// WebsiteRoot returns the public website base URL.  This is taken from the
+// GOVUK_WEBSITE_ROOT environment variable.  If this is unset, an EnvVarMissing
+// error will be returned.  If it can't be parsed by url.Parse, an
+// EnvVarURLInvalid error will be returned.
 func WebsiteRoot() (*url.URL, error) {
 	return parseEnvVarURL("GOVUK_WEBSITE_ROOT")
 }
 
+// AssetRoot returns the public assets base URL. This is taken from the
+// GOVUK_ASSET_ROOT environment variable.  If this is unset, an EnvVarMissing
+// error will be returned.  If it can't be parsed by url.Parse, an
+// EnvVarURLInvalid error will be returned.
 func AssetRoot() (*url.URL, error) {
 	return parseEnvVarURL("GOVUK_ASSET_ROOT")
 }
