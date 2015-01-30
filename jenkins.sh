4504106379bfac0918e5f88e116bdc45c4c633ad
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-01-30 16:51:33 +0000

    Configure for jenkins branch builds

--- jenkins.sh
+++ jenkins.sh
@@ -3,4 +3,7 @@ set -e
 rm -f Gemfile.lock
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 bundle exec rake
-bundle exec rake publish_gem
+
+if [[ -n "$PUBLISH_GEM" ]]; then
+  bundle exec rake publish_gem
+fi
