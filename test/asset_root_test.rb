3736c3c5446f22d99a8503b92b753c3a94f3d334
Paul Bowsher <paul.bowsher@gmail.com>
2016-02-04 12:18:10 +0000

    Tidy up test names and descriptions for asset_root

--- test/asset_root_test.rb
+++ test/asset_root_test.rb
@@ -1,15 +1,15 @@
 require_relative "test_helper"
 
 describe Plek do
-  describe "retreiving the asset_root" do
-    it "should return the GOVUK_ASSET_ROOT env variable" do
+  describe "asset_root" do
+    it "returns the GOVUK_ASSET_ROOT env variable" do
       ClimateControl.modify GOVUK_ASSET_ROOT: "http://static.dev.gov.uk" do
         assert_equal "http://static.dev.gov.uk", Plek.new("foo.gov.uk").asset_root
       end
     end
 
     describe "When GOVUK_ASSET_ROOT env variable isn't set" do
-      it "should raise an exception if RAILS_ENV is production" do
+      it "raises an exception if RAILS_ENV is production" do
         ClimateControl.modify RAILS_ENV: "production" do
           assert_raises Plek::NoConfigurationError do
             Plek.new("foo.gov.uk").asset_root
@@ -17,7 +17,7 @@ describe Plek do
         end
       end
 
-      it "should raise an exception if RACK_ENV is production" do
+      it "raises an exception if RACK_ENV is production" do
         ClimateControl.modify RACK_ENV: "production" do
           assert_raises Plek::NoConfigurationError do
             Plek.new("foo.gov.uk").asset_root
@@ -25,7 +25,7 @@ describe Plek do
         end
       end
 
-      it "should return find('static') otherwise" do
+      it "returns find('static') otherwise" do
         assert_equal "https://static.foo.gov.uk", Plek.new("foo.gov.uk").asset_root
       end
     end
